ea1a8edcefcda9a93a066749a03d2b640b5251ea
Lukasz <lukasz.gras.97@gmail.com>
2020-12-16 20:12:51 +0100

    Decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import React from "react";
+import {ThemeProvider,theme,CSSReset,Box} from "@chakra-ui/react";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -5,4 +7,15 @@ export const parameters = {
     storySort: (a, b) =>
         a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+      <ThemeProvider theme={theme}>
+          <CSSReset/>
+          <Box m="4">
+            <Story/>
+          </Box>
+      </ThemeProvider>
+  )
+];
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -3,11 +3,23 @@ import Input from "./Input";
 
 export default {
     title: 'form/Input',
-    component: Input
+    component: Input,
+    onClick: {action: 'clicked'}
 }
 
 export const Small = () => <Input variant='small'/>;
 export const Normal = () => <Input variant='normal'/>;
 export const Big = () => <Input variant='big'/>;
+
+
+const Template = args => <Input {...args}/>
+export const SmallInput = Template.bind({});
+SmallInput.args = {
+    variant:'small',
+    text: 'args small input',
+
+};
+
+
 
 Small.storyName = 'Small name';
